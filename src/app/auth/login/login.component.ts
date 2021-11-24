d8e37c63bb4afc29e1097b274d0a1686e3606b3d
Olerg <schara@mail.ru>
2021-11-24 14:48:36 +0300

    авторизация

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Message } from 'src/app/shared/models/message.model';
 import { User } from 'src/app/shared/models/user.model';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { UsersService } from 'src/app/shared/services/users.service';
 
 @Component({
@@ -14,7 +16,11 @@ export class LoginComponent implements OnInit {
     form!: FormGroup;
     message!: Message;
 
-    constructor(private usersService: UsersService) { }
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService,
+        private router: Router
+    ) { }
 
     ngOnInit(): void {
         this.message = new Message('danger', '');
@@ -39,7 +45,10 @@ export class LoginComponent implements OnInit {
             .subscribe((user: User) => {
                 if (user) {
                     if (user.password === formData.password) {
-                        //logic
+                        this.message.text = '';
+                        window.localStorage.setItem('user',JSON.stringify(user));
+                        this.authService.login();
+                        //this.router.navigate(['']);
                     } else {
                         this.showMessage('Пароль не верный');
                     }
