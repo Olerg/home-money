eb9a56e456e9ebaada074761a5adee0b3f1274b2
Olerg <schara@mail.ru>
2021-11-26 12:40:30 +0300

    валидация асинхронная

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -16,7 +16,7 @@ export class RegistrationComponent implements OnInit {
 
     ngOnInit() {
         this.form = new FormGroup({
-            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'email': new FormControl(null, [Validators.required, Validators.email]/*, this.forbiddenEmails.bind(this)*/),
             'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
             'name': new FormControl(null, [Validators.required]),
             'agree': new FormControl(false, [Validators.requiredTrue])
@@ -24,6 +24,7 @@ export class RegistrationComponent implements OnInit {
     }
 
     onSubmit() {
+        console.log(this.form);
         const { email, password, name } = this.form.value;
         const user = new User(email, password, name);
         this.usersService.createNewUser(user)
@@ -35,5 +36,19 @@ export class RegistrationComponent implements OnInit {
                 });
             });
     }
+
+    /*forbiddenEmails(control: FormControl): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.usersService.getUserByEmail(control.value)
+                .subscribe((user: User) => {
+                    console.log(user);
+                    if (user) {
+                        resolve({ forbiddenEmail: true });
+                    } else {
+                        resolve(null);
+                    }
+                });
+        });
+    }*/
 
 }
