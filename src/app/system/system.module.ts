c2714b170d2e616698225c71ed14991e69399275
Olerg <schara@mail.ru>
2021-11-26 13:40:55 +0300

    создание модуля системы

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -1,1 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { SharedModule } from "../shared/shared.module";
+import { SystemRoutingModule } from "./system-routing.module";
+import { BillPageComponent } from './bill-page/bill-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { PlanningPageComponent } from './planning-page/planning-page.component';
+import { RecordsPageComponent } from './records-page/records-page.component';
 
+@NgModule({
+    imports: [CommonModule, SharedModule, SystemRoutingModule],
+    declarations: [
+      BillPageComponent,
+      HistoryPageComponent,
+      PlanningPageComponent,
+      RecordsPageComponent
+    ]
+})
+export class SystemModule { }
