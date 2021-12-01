85ac90ddefb4cbbeaef94eaff925b4ef3775c871
Olerg <schara@mail.ru>
2021-12-01 12:41:07 +0300

    страница записей

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { User } from "../models/user.model";
@@ -13,8 +12,11 @@ export class UsersService extends BaseApi {
     }
 
 
-    getUserByEmail(email: string): Observable<User> {
-        return this.get(`users?email=${email}`);
+    getUserByEmail(email: string): Observable<any> {
+        return this.get(`users?email=${email}`)
+            .pipe(
+                map((users: User[]) => users[0] ? users[0] : undefined)
+            );
     }
 
     createNewUser(user: User): Observable<User> {
