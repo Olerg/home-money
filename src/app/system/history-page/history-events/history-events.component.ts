b8f808f4991877ddc6e1ff0a1b8f6887c34bca76
Olerg <schara@mail.ru>
2021-12-03 16:31:43 +0300

    фильтр

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -11,6 +11,9 @@ export class HistoryEventsComponent implements OnInit {
 
     @Input() categories: Category[] = [];
     @Input() events: WFMEvent[] = [];
+    searchValue = '';
+    searchPlaceholder = 'Сумма';
+    searchField = 'amount';
 
     constructor() { }
 
@@ -26,6 +29,17 @@ export class HistoryEventsComponent implements OnInit {
             'label-danger': e.type === 'outcome',
             'label-success': e.type === 'income'
         };
+    }
+
+    changeCriteria(field: string) {
+        const namesMap:any = {
+            amount: 'Сумма',
+            date: 'Дата',
+            category: 'Категория',
+            type: 'Тип'
+        };
+        this.searchPlaceholder = namesMap[field];
+        this.searchField = field;
     }
 
 }
