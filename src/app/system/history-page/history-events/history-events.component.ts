adfcc3815033b3ca8f017306523fb8a7251be970
Olerg <schara@mail.ru>
2021-12-03 15:52:42 +0300

    список

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../../shared/models/category.model';
+import { WFMEvent } from '../../shared/models/event.model';
 
 @Component({
-  selector: 'wfm-history-events',
-  templateUrl: './history-events.component.html',
-  styleUrls: ['./history-events.component.scss']
+    selector: 'wfm-history-events',
+    templateUrl: './history-events.component.html',
+    styleUrls: ['./history-events.component.scss']
 })
 export class HistoryEventsComponent implements OnInit {
 
-  constructor() { }
+    @Input() categories: Category[] = [];
+    @Input() events: WFMEvent[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.events.forEach((e) => {
+            e.catName = this.categories.find(c => c.id === e.category)?.name;
+        });
+    }
+
+    getEventClass(e: WFMEvent) {
+        return {
+            'label': true,
+            'label-danger': e.type === 'outcome',
+            'label-success': e.type === 'income'
+        };
+    }
 
 }
