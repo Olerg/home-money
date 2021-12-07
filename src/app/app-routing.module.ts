601441008f5a9ded307199adf198075a6926ad40
Olerg <schara@mail.ru>
2021-12-07 14:19:49 +0300

    страница 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { NotFoundComponent } from "./shared/components/not-found/not-found.component";
 
 const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     {
         path: 'system', loadChildren: () => import('./system/system.module').then(
             m => m.SystemModule)
-    }
+    },
+    {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { NgxChartsModule } from "@swimlane/ngx-charts";
 
+
 @NgModule({
     imports: [ReactiveFormsModule, FormsModule, NgxChartsModule],
     exports: [ReactiveFormsModule, FormsModule, NgxChartsModule]
